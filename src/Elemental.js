ede23ec18df56661dc13486bd3df83e4566b608e
Jed Watson <jed.watson@me.com>
2015-04-15 12:46:51 +1000

    Fixing paths

---
+++ src/Elemental.js
@@ -0,0 +1,5 @@
+exports.EmailInputGroup = require('./components/EmailInputGroup');
+exports.PasswordInputGroup = require('./components/PasswordInputGroup');
+exports.RadioGroup = require('./components/RadioGroup');
+exports.SelectInputGroup = require('./components/SelectInputGroup');
+exports.Spinner = require('./components/Spinner');
--- src/index.js
+++
@@ -1,5 +0,0 @@
-exports.EmailInputGroup = require('components/EmailInputGroup');
-exports.PasswordInputGroup = require('components/PasswordInputGroup');
-exports.RadioGroup = require('components/RadioGroup');
-exports.SelectInputGroup = require('components/SelectInputGroup');
-exports.Spinner = require('components/Spinner');
