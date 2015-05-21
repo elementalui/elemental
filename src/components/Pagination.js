d1d67c5158a901fe61f4e9d57210f2339175a1ae
Joss Mackison <joss.mackison@gmail.com>
2015-05-21 10:48:13 +1000

    unnecessary array; blacklist accepts infinite arguments

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -44,7 +44,7 @@ module.exports = React.createClass({
 	},
 	render() {
 		// props
-		var props = blacklist(this.props, ['className']);
+		var props = blacklist(this.props, 'className');
 
 		
 		// classes
