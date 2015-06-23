bde61c251bb99af4aac823ddd69be8c083d747c9
Joss Mackison <joss.mackison@gmail.com>
2015-06-23 18:41:32 +1000

    remove cruft from FormField

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -5,23 +5,9 @@ var classNames = require('classnames');
 module.exports = React.createClass({
 	displayName: 'FormField',
 	propTypes: {
-		className: React.PropTypes.string,
-		disabled: React.PropTypes.bool,
-		htmlFor: React.PropTypes.string,
-		id: React.PropTypes.string,
 		label: React.PropTypes.string,
-		multiline: React.PropTypes.bool,
 		offsetAbsentLabel: React.PropTypes.bool,
-		onChange: React.PropTypes.func,
-		size: React.PropTypes.oneOf(['lg', 'sm', 'xs']),
-		type: React.PropTypes.string,
-		value: React.PropTypes.string,
 		width: React.PropTypes.oneOf(['one-half', 'two-quarters', 'three-sixths', 'one-quarter', 'three-quarters', 'one-third', 'two-sixths', 'two-thirds', 'four-sixths', 'one-fifth', 'two-fifths', 'three-fifths', 'four-fifths', 'one-sixth', 'five-sixths'])
-	},
-	getDefaultProps() {
-		return {
-			type: 'text'
-		};
 	},
 	render() {
 		// classes
@@ -31,7 +17,7 @@ module.exports = React.createClass({
 
 
 		// props
-		var props = blacklist(this.props, 'className', 'label', 'type', 'width');
+		var props = blacklist(this.props, 'className', 'label', 'offsetAbsentLabel', 'width');
 
 
 		// elements
