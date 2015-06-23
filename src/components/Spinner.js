93c661cb7d9d10a2d3e0c805f8cab7782ee3378f
Joss Mackison <joss.mackison@gmail.com>
2015-06-23 15:54:29 +1000

    tidy up spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,13 +4,11 @@ var classNames = require('classnames');
 module.exports = React.createClass({
 	displayName: 'Spinner',
 	propTypes: {
-		className: React.PropTypes.string,
 		size: React.PropTypes.oneOf(['sm', 'md', 'lg']),
 		type: React.PropTypes.oneOf(['default', 'primary', 'inverted'])
 	},
 	getDefaultProps() {
 		return {
-			size: 'md',
 			type: 'default'
 		};
 	},
@@ -24,9 +22,9 @@ module.exports = React.createClass({
 
 		return (
 			<div className={componentClass}>
-				<i className="Spinner_dot Spinner_dot--first" />
-				<i className="Spinner_dot Spinner_dot--second" />
-				<i className="Spinner_dot Spinner_dot--third" />
+				<span className="Spinner_dot Spinner_dot--first" />
+				<span className="Spinner_dot Spinner_dot--second" />
+				<span className="Spinner_dot Spinner_dot--third" />
 			</div>
 		);
 	}
