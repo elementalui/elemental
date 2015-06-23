07639b8c23b20bf692136a2c22a886f669b3d33d
Joss Mackison <joss.mackison@gmail.com>
2015-06-23 18:17:53 +1000

    FormRow: unnecessary propType: "className"

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -3,9 +3,6 @@ var classNames = require('classnames');
 
 module.exports = React.createClass({
 	displayName: 'FormRow',
-	propTypes: {
-		className: React.PropTypes.string
-	},
 	render() {
 		// classes
 		var className = classNames('form-row', this.props.className);
