710fadc0507bcaf1136a72ccea696f8bde4f92ef
Joss Mackison <joss.mackison@gmail.com>
2015-08-21 16:39:35 +1000

    move pagination props to top level

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,48 +5,44 @@ var classNames = require('classnames');
 module.exports = React.createClass({
 	displayName: 'Pagination',
 	propTypes: {
-		className: React.PropTypes.string,
-		onClick: React.PropTypes.func,
-		pagination: React.PropTypes.shape({
-			currentPage: React.PropTypes.number,
-			first:       React.PropTypes.number,
-			last:        React.PropTypes.number,
-			next:        React.PropTypes.bool,
-			pages:       React.PropTypes.array,
-			previous:    React.PropTypes.bool,
-			total:       React.PropTypes.number,
-			totalPages:  React.PropTypes.number
-		})
+		className:    React.PropTypes.string,
+		currentPage:  React.PropTypes.number,
+		first:        React.PropTypes.number,
+		last:         React.PropTypes.number,
+		next:         React.PropTypes.bool,
+		onPageSelect: React.PropTypes.func,
+		pages:        React.PropTypes.array,
+		previous:     React.PropTypes.bool,
+		total:        React.PropTypes.number,
+		totalPages:   React.PropTypes.number,
 	},
 	renderCount() {
-		var count = this.props.pagination;
+		var { first, last, total } = this.props;
 		return (
-			<div className="Pagination__count">Showing {count.first} to {count.last} of {count.total}</div>
+			<div className="Pagination__count">Showing {first} to {last} of {total}</div>
 		);
 	},
 	renderList() {
-		if (!this.props.pagination.pages) return 'No pages...';
+		let { currentPage, onPageSelect, pages} = this.props;
+		if (!pages) return 'No pages...';
 
-		var self = this;
-
-		var pages = this.props.pagination.pages.map(function(page) {
+		var list = pages.map((pg) => {
 			var className = classNames('Pagination__list__item', {
-				'is-selected': self.props.pagination.currentPage === page
+				'is-selected': currentPage === pg
 			});
-			return <button key={'page_' + page} className={className} onClick={self.props.onClick}>{page}</button>;
+			return <button key={'page_' + pg} className={className} onClick={onPageSelect}>{pg}</button>;
 		});
 
 		return (
 			<div className="Pagination__list">
-				{pages}
+				{list}
 			</div>
 		);
 	},
 	render() {
 		// props
 		var props = blacklist(this.props, 'className');
-
-
+		
 		// classes
 		var componentClass = classNames('Pagination', this.props.className);
 		props.className = componentClass;
