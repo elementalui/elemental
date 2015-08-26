6ca2b38471e451217d2b3266ef06af69e4a708b8
Joss Mackison <joss@thinkmill.com.au>
2015-08-27 01:33:30 +1000

    add some colours to constants
    plus alignment

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,19 @@
 exports.breakpoint = {
-	xs:            480,
-	sm:            768,
-	md:            992,
-	lg:           1200,
+	xs:                      480,
+	sm:                      768,
+	md:                      992,
+	lg:                     1200,
 };
+exports.color = {
+	appDanger:              '#d64242',
+	appInfo:                '#56cdfc',
+	appPrimary:             '#1385e5',
+	appSuccess:             '#34c240',
+	appWarning:             '#fa9f47',
+	brandPrimary:           '#31adb8',
+};
+
 exports.width = {
-	container:    1170,
-	gutter:         20,
+	container:               1170,
+	gutter:                    20,
 };
