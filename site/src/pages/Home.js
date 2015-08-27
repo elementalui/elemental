810e267932488b7e5160d57f7929f2c020d60e9d
Jed Watson <jed.watson@me.com>
2015-08-27 23:57:37 +1000

    Updating homepage to use new Grid system

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,5 +1,5 @@
-var React = require('react');
-var Router = require('react-router');
+const React = require('react');
+const Router = require('react-router');
 
 const NavItems = [
 	{ value: 'css',     icon: 'paintcan',      label: 'CSS' },
@@ -11,26 +11,28 @@ const NavItems = [
 	// { value: 'date-picker', icon: 'calendar', label: 'Date' }
 ];
 
+const { Col, Container, Row } = require('elemental');
+
 var Home = React.createClass({
 	displayName: 'VIEW_Home',
 
 	render () {
 		var menuItems = NavItems.map(function(item) {
 			return (
-				<div key={item.label} className="demo-banner-nav__col col-xs-4 col-sm-2">
+				<Col xs="33.333%" sm="16.6666667%" key={item.label} className="demo-banner-nav__col col-xs-4 col-sm-2">
 					<Router.Link key={item.value} className="demo-banner-nav__item" onClick={self.toggleMenu} to={item.value}>
 						<span className={'demo-banner-nav__icon octicon octicon-' + item.icon} />
 						<div className="demo-banner-nav__label">
 							<span className="demo-banner-nav__label-inner">{item.label}</span>
 						</div>
 					</Router.Link>
-				</div>
+				</Col>
 			);
 		});
 		return (
 			<div>
 				<header className="demo-banner demo-banner--primary">
-					<div className="demo-container container">
+					<Container className="demo-container">
 						<span className="demo-banner-illustration" />
 						<h1 className="demo-banner__heading demo-banner__heading-1">Elemental UI</h1>
 						<h2 className="demo-banner__heading demo-banner__heading-2">A UI Toolkit for React.js Websites and Apps</h2>
@@ -38,10 +40,10 @@ var Home = React.createClass({
 							<a className="Button Button--demo-primary" href="https://twitter.com/elementalui" target="_blank">Follow @ElementalUI on Twitter</a>
 							<a className="Button Button--demo-link" href="https://github.com/elementalui/elemental" target="_blank">View the GitHub Project</a>
 						</div>
-					</div>
+					</Container>
 				</header>
 				<div className="demo-banner demo-banner--secondary">
-					<div className="demo-container container">
+					<Container className="demo-container">
 						<h2 className="demo-banner__heading demo-banner__heading-2">Project Status</h2>
 						<ul className="demo-banner-list">
 							<li>Currently under heavy development, initially for use in <a href="http://www.keystonejs.com">KeystoneJS</a></li>
@@ -51,13 +53,13 @@ var Home = React.createClass({
 						<h5 className="demo-banner-divider">
 							<span className="demo-banner-divider-inner">Demos</span>
 						</h5>
-						<div className="demo-banner-nav row">
+						<Row className="demo-banner-nav">
 							{menuItems}
-						</div>
-					</div>
+						</Row>
+					</Container>
 				</div>
 				<div className="demo-banner demo-banner--tertiary">
-					<div className="demo-container container">
+					<Container className="demo-container">
 						<h2 className="demo-banner__heading demo-banner__heading-2">Why build <em>another</em> UI kit?</h2>
 						<p>At <a href="http://www.thinkmill.com.au" target="_blank">Thinkmill</a> we are constantly building bespoke web applications for various clients. We noticed that whilst projects vary greatly, we're using the same CSS and Component foundations over and over again.</p>
 						<p>In an effort to keep our projects' codebases <abbr title="Don't Repeat Yourself">DRY</abbr> we decided it was time to build something to improve our workflow and unify our projects to decrease development time.</p>
@@ -65,22 +67,22 @@ var Home = React.createClass({
 						<p>While there are many other great UI Libraries available (including for React), our goal with Elemental is to develop a light-weight, unopinionated, modular framework that we can use across all our web projects.</p>
 
 						<div className="demo-banner-points">
-							<div className="row">
-								<div className="col-sm-4">
+							<Row>
+								<Col sm="33.333%">
 									<h3>Open Source</h3>
 									<p>Available for use under the MIT license,  built on foundations of React.js, LESS, Babel and Gulp, and inspired by other great projects.</p>
-								</div>
-								<div className="col-sm-4">
+								</Col>
+								<Col sm="33.333%">
 									<h3>Modern Workflows</h3>
 									<p>Elemental is designed to be installed from npm and built into your project with browserify or webpack. You can customise it by including our LESS too.</p>
-								</div>
-								<div className="col-sm-4">
+								</Col>
+								<Col sm="33.333%">
 									<h3>Made by Thinkmill</h3>
 									<p>Elemental UI is the cornerstone of Thinkmill's development suite, made by people who share a passion for HTML, CSS and JavaScript.</p>
-								</div>
-							</div>
+								</Col>
+							</Row>
 						</div>
-					</div>
+					</Container>
 				</div>
 			</div>
 		);
