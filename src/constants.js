e4508c023c673b5cfb4dfc60b390dc7e43b1e4af
Jed Watson <jed.watson@me.com>
2015-08-28 00:42:09 +1000

    Supporting more fractions. Maths!

--- src/constants.js
+++ src/constants.js
@@ -46,24 +46,16 @@ function perc(n) {
 	return (n * 100) + '%';
 }
 
-exports.fractions = {
-	'1/2':      perc(1/2),
-	'1/3':      perc(1/3),
-	'1/4':      perc(1/4),
-	'1/5':      perc(1/5),
-	'1/6':      perc(1/6),
-	'1/7':      perc(1/7),
-	'1/8':      perc(1/8),
-	'1/9':      perc(1/9),
-	'1/10':    perc(1/10),
-	'1/11':    perc(1/11),
-	'1/12':    perc(1/12),
-	'1/13':    perc(1/13),
-	'1/14':    perc(1/14),
-	'1/15':    perc(1/15),
-	'1/16':    perc(1/16),
-	'1/17':    perc(1/17),
-	'1/18':    perc(1/18),
-	'1/19':    perc(1/19),
-	'1/20':    perc(1/20),
-};
+function denominators (n) {
+	for (var d = 2; d <= 20; d++) {
+		if (n < d) {
+			exports.fractions[n + '/' + d] = perc(n / d);
+		}
+	}
+}
+
+exports.fractions = {};
+
+for (var numerator = 1; numerator <= 19; numerator++) {
+	denominators(numerator);
+}
