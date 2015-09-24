52a0cbc14e3c67f709d3c7de9a24a51d74b24ff8
Zach <zackify@gmail.com>
2015-09-23 17:24:02 -0700

    updated to legit tests 0.4.2 - added .element

--- tests/components/pagination.js
+++ tests/components/pagination.js
@@ -5,8 +5,8 @@ import Pagination from '../../lib/components/Pagination';
 
 describe('Component: Pagination', function () {
 
-  let children = (component) => {
-    return component.props.children.map(child => {
+  let children = (list) => {
+    return list.props.children.map(child => {
       return child.props.children;
     });
   };
@@ -17,8 +17,8 @@ describe('Component: Pagination', function () {
 
       Test(<Pagination currentPage={1} pageSize={10} total={30}/>)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(Pagination__list.props.children).to.have.length(3);
+      .element(list => {
+        expect(list.props.children).to.have.length(3);
       });
     });
 
@@ -30,16 +30,16 @@ describe('Component: Pagination', function () {
 
       Test(<Pagination currentPage={2} pageSize={10} total={30}/>)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(Array.from(Pagination__list.props.children).filter(classFilter)[0].props.children).to.equal(2);
+      .element(list => {
+        expect(Array.from(list.props.children).filter(classFilter)[0].props.children).to.equal(2);
       });
     });
 
     it('renders all pages', function () {
       Test(<Pagination currentPage={1} pageSize={10} total={45}/>)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(children(Pagination__list)).to.eql([1, 2, 3, 4, 5]);
+      .element(list => {
+        expect(children(list)).to.eql([1, 2, 3, 4, 5]);
       });
     });
 
@@ -50,8 +50,8 @@ describe('Component: Pagination', function () {
     it('renders correctly when on first page', function () {
       Test(<Pagination currentPage={1} pageSize={10} total={100} limit={6} />)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(children(Pagination__list)).to.eql([1, 2, 3, 4, 5, 6, 7, '...']);
+      .element(list => {
+        expect(children(list)).to.eql([1, 2, 3, 4, 5, 6, 7, '...']);
       });
 
     });
@@ -59,40 +59,40 @@ describe('Component: Pagination', function () {
     it('renders correctly when on last page', function () {
       Test(<Pagination currentPage={10} pageSize={10} total={100} limit={6} />)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(children(Pagination__list)).to.eql(['...', 4, 5, 6, 7, 8, 9, 10]);
+      .element(list => {
+        expect(children(list)).to.eql(['...', 4, 5, 6, 7, 8, 9, 10]);
       });
     });
 
     it('renders correctly when on last page and it is not filled in full', function () {
       Test(<Pagination currentPage={111} pageSize={10} total={105} limit={6} />)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(children(Pagination__list)).to.eql(['...', 5, 6, 7, 8, 9, 10, 11]);
+      .element(list => {
+        expect(children(list)).to.eql(['...', 5, 6, 7, 8, 9, 10, 11]);
       });
     });
 
     it('limits number of pages in the middle of pagination (odd number)', function () {
       Test(<Pagination currentPage={10} pageSize={25} total={1000} limit={5} />)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(children(Pagination__list)).to.eql(['...', 8, 9, 10, 11, 12, '...']);
+      .element(list => {
+        expect(children(list)).to.eql(['...', 8, 9, 10, 11, 12, '...']);
       });
     });
 
     it('limits number of pages in the middle of pagination (even number)', function () {
       Test(<Pagination currentPage={10} pageSize={25} total={1000} limit={4} />)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(children(Pagination__list)).to.eql(['...', 8, 9, 10, 11, 12, '...']);
+      .element(list => {
+        expect(children(list)).to.eql(['...', 8, 9, 10, 11, 12, '...']);
       });
     });
 
     it('renders correctly when limit is extremely small', function () {
       Test(<Pagination currentPage={4} pageSize={36} total={130} limit={2} />)
       .find('.Pagination__list')
-      .test(({Pagination__list}) => {
-        expect(children(Pagination__list)).to.eql(['...', 2, 3, 4]);
+      .element(list => {
+        expect(children(list)).to.eql(['...', 2, 3, 4]);
       });
     });
 
