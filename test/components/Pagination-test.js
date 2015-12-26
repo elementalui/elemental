9e7fc3bfb0c32ec4ff90622c2e4d6d398a9e575f
Nik Butenko <nik@butenko.me>
2015-12-26 11:48:38 +1100

    Use teaspoon for selections

--- test/components/Pagination-test.js
+++ test/components/Pagination-test.js
@@ -1,87 +1,97 @@
 import { test } from 'tape';
 import Pagination from '../../lib/components/Pagination';
 import React from 'react/addons';
-const TestUtils = React.addons.TestUtils;
-
-/**
- * TODO: move to utils for re-use
- */
-const createComponent = (component, props, ...children) => {
-	const shallowRenderer = TestUtils.createRenderer();
-	shallowRenderer.render(
-		React.createElement(component, props, children.length === 1 ? children[0] : children)
-	);
-	return shallowRenderer.getRenderOutput();
-};
+import $ from 'teaspoon';
 
 
-const getList = props => createComponent(Pagination, props).props.children[1];
+const getList = props => $(<Pagination {...props} />)
+	.shallowRender()
+	.children('.Pagination__list');
+
+
+const getPageNumbers = props => getList(props)
+	.children()
+	.get()
+	.map(node => node.props.children);
 
 
 test('Pagination. Basics', t => {
 
-	t.ok(createComponent(Pagination, { currentPage: 1, pageSize: 10, total: 30 })
-			.props.className.indexOf('Pagination') !== -1,
+	t.ok(
+		$(<Pagination {...{ currentPage: 1, pageSize: 10, total: 30 }} />)
+			.shallowRender()
+			.is('.Pagination'),
 		'should create .Pagination container');
 
 
-	t.ok(getList({ currentPage: 1, pageSize: 10, total: 30 })
-			.props.className.indexOf('Pagination__list') !== -1,
+	t.equal(
+		getList({ currentPage: 1, pageSize: 10, total: 30 })
+			.length,
+		1,
 		'should create .Pagination__list container for pages');
 
 
-	t.equal(getList({ currentPage: 1, pageSize: 10, total: 30 })
-			.props.children.length, 3,
+	t.equal(
+		getList({ currentPage: 1, pageSize: 10, total: 30 })
+			.children().length,
+		3,
 		'should render 3 pages for 30 items with 10 items per page');
 
 
-	t.ok(getList({ currentPage: 1, pageSize: 10, total: 30 })
-			.props.children[0].props.className.indexOf('Pagination__list__item') !== -1,
+	t.equal(getList({ currentPage: 1, pageSize: 10, total: 30 })
+		.children('.Pagination__list__item').length,
+		3,
 		'should create .Pagination__list__item containers for each page');
 
 
-	t.ok(getList({ currentPage: 2, pageSize: 10, total: 30 })
-			.props.children[1].props.className.indexOf('is-selected') !== -1,
+	t.equal(getList({ currentPage: 2, pageSize: 10, total: 30 })
+		.children('.is-selected').length,
+		1,
 		'should add "is-selected" class to current page element');
 
-
-	t.deepEqual(getList({ currentPage: 1, pageSize: 10, total: 45 })
-		.props.children.map(page => page.props.children), [1, 2, 3, 4, 5],
+	t.deepEqual(
+		getPageNumbers({ currentPage: 1, pageSize: 10, total: 45 }),
+		[1, 2, 3, 4, 5],
 		'should render page numbers');
-
 
 	t.end();
 });
 
 test('Pagination. Limit', t => {
 
-	t.deepEqual(getList({ currentPage: 1, pageSize: 10, total: 100, limit: 6 })
-		.props.children.map(page => page.props.children), [1, 2, 3, 4, 5, 6, '...'],
+	t.deepEqual(
+		getPageNumbers({ currentPage: 1, pageSize: 10, total: 100, limit: 6 }),
+		[1, 2, 3, 4, 5, 6, '...'],
 		'should limit number of first pages when on first page');
 
 
-	t.deepEqual(getList({ currentPage: 10, pageSize: 10, total: 100, limit: 6 })
-		.props.children.map(page => page.props.children), ['...', 5, 6, 7, 8, 9, 10],
+	t.deepEqual(
+		getPageNumbers({ currentPage: 10, pageSize: 10, total: 100, limit: 6 }),
+		['...', 5, 6, 7, 8, 9, 10],
 		'should limit number of last pages when on the last page');
 
 
-	t.deepEqual(getList({ currentPage: 111, pageSize: 10, total: 105, limit: 6 })
-		.props.children.map(page => page.props.children), ['...', 6, 7, 8, 9, 10, 11],
+	t.deepEqual(
+		getPageNumbers({ currentPage: 111, pageSize: 10, total: 105, limit: 6 }),
+		['...', 6, 7, 8, 9, 10, 11],
 		'should limit number of last pages when on the last page and it is not filled in full');
 
 
-	t.deepEqual(getList({ currentPage: 10, pageSize: 25, total: 1000, limit: 5 })
-		.props.children.map(page => page.props.children), ['...', 8, 9, 10, 11, 12, '...'],
+	t.deepEqual(
+		getPageNumbers({ currentPage: 10, pageSize: 25, total: 1000, limit: 5 }),
+		['...', 8, 9, 10, 11, 12, '...'],
 		'should limit number of pages in the middle of pagination (odd number)');
 
 
-	t.deepEqual(getList({ currentPage: 10, pageSize: 25, total: 1000, limit: 4 })
-		.props.children.map(page => page.props.children), ['...', 9, 10, 11, 12, '...'],
+	t.deepEqual(
+		getPageNumbers({ currentPage: 10, pageSize: 25, total: 1000, limit: 4 }),
+		['...', 9, 10, 11, 12, '...'],
 		'should limit number of pages in the middle of pagination (even number)');
 
 
-	t.deepEqual(getList({ currentPage: 4, pageSize: 36, total: 130, limit: 2 })
-		.props.children.map(page => page.props.children), ['...', 3, 4],
+	t.deepEqual(
+		getPageNumbers({ currentPage: 4, pageSize: 36, total: 130, limit: 2 }),
+		['...', 3, 4],
 		'should render correctly when limit is extremely small');
 
 
